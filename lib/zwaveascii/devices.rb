15b9c93b77a553260bb8d9f572803929f9edd044
sen <github@sen.cx>
2015-06-14 10:52:53 -0400

    Adding support for battery powered node status.

--- lib/zwaveascii/devices.rb
+++ lib/zwaveascii/devices.rb
@@ -6,6 +6,17 @@ module ZWave
     end
   end
   
+  class BatteryNode < Node
+    def initialize(controller, address)
+      super controller, address
+    end
+    
+    def battery_level
+      response = @controller.send_cmd ">N#{@address}SS128,2", ["E", "N", "X", "N", "n"] 
+      response[2].split(",")[3].to_i
+    end
+  end
+  
   class Switch < Node
     def initialize(controller, address)
       super controller, address
@@ -36,6 +47,26 @@ module ZWave
     def level
       # map 0-99 to 0-100 so we output percentage
       @controller.get_level(@address) * 100 / 99;
+    end
+  end
+
+  class DoorLock < BatteryNode
+    def initialize(controller, address)
+      super controller, address
+    end
+
+    def locked?
+      response = @controller.send_cmd ">N#{@address}SS98,2", ["E", "N", "X", "N", "n"]
+      return true if response[2].split(",")[3].to_i != 0
+      false
+    end
+
+    def lock
+      @controller.send_cmd ">N#{@address}SS98,1,255", ["E", "N", "X", "N", "n"] 
+    end
+
+    def unlock
+      @controller.send_cmd ">N#{@address}SS98,1,0", ["E", "N", "X", "N", "n"]
     end
   end
 end
