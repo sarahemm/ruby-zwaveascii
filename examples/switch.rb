289868f42a2c20c531e385f4a945428ef6ac354a
sen <github@sen.cx>
2015-06-14 10:53:16 -0400

    Adding some extra debug messaging and fixing the speed.

--- examples/switch.rb
+++ examples/switch.rb
@@ -4,7 +4,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'zwaveascii'
 
-zwave = ZWave::ASCII.new ARGV[0]
+zwave = ZWave::ASCII.new ARGV[0], 115200
 zwave.debug = true
 unit_id = ARGV[1].to_i
 puts "\n-----\n\n"
@@ -13,6 +13,7 @@ puts "SWITCHING UNIT #{unit_id} OFF"
 switch = zwave.switch(unit_id)
 switch.on = false
 
+puts "Sleeping for 2 seconds..."
 sleep 2
 
 puts "SWITCHING UNIT #{unit_id} ON"
